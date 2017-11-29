b33b60728896788861825263490ffcc5aaac741d
bgpat <bgpat@users.noreply.github.com>
2017-11-29 17:55:42 +0900

    Implement pool

---
+++ ippool.go
@@ -0,0 +1,131 @@
+package ippool
+
+import (
+	"fmt"
+	"net"
+)
+
+type Pool struct {
+	Entire  Range
+	Remains []Range
+}
+
+type Range struct {
+	First net.IP
+	Last  net.IP
+}
+
+func prevIP(ip net.IP) net.IP {
+	prev := make(net.IP, len(ip))
+	copy(prev, ip)
+	for i := len(prev) - 1; i > 0; i-- {
+		if prev[i] > 0 {
+			prev[i]--
+			return prev
+		}
+		prev[i] = 0xff
+	}
+	return nil
+}
+
+func nextIP(ip net.IP) net.IP {
+	next := make(net.IP, len(ip))
+	copy(next, ip)
+	for i := len(next) - 1; i > 0; i-- {
+		if next[i] < 0xff {
+			next[i]++
+			return next
+		}
+		next[i] = 0
+	}
+	return nil
+}
+
+func compareIP(a, b net.IP) int {
+	x := a.To16()
+	y := b.To16()
+	for i := 0; i < len(x); i++ {
+		if x[i] < y[i] {
+			return -1
+		}
+		if x[i] > y[i] {
+			return 1
+		}
+	}
+	return 0
+}
+
+func NewPool(first, last net.IP) *Pool {
+	if compareIP(first, last) > 0 {
+		return nil
+	}
+	return &Pool{
+		Entire: Range{
+			First: first.To16(),
+			Last:  last.To16(),
+		},
+		Remains: []Range{
+			Range{
+				First: first.To16(),
+				Last:  last.To16(),
+			},
+		},
+	}
+}
+
+func (p *Pool) Clean() {
+	tmp := make([]Range, 0)
+	for _, r := range p.Remains {
+		if r.Count() > 0 {
+			tmp = append(tmp, r)
+		}
+	}
+	p.Remains = tmp
+}
+
+func (p *Pool) Contain(ip net.IP) bool {
+	if !p.Entire.Contain(ip) {
+		return false
+	}
+	for _, r := range p.Remains {
+		if r.Contain(ip) {
+			return true
+		}
+	}
+	return false
+}
+
+func (p *Pool) Allocate(ip net.IP) error {
+	if !p.Entire.Contain(ip) {
+		return fmt.Errorf("%s is out of pool range", ip.String())
+	}
+	for i, r := range p.Remains {
+		if r.Contain(ip) {
+			p.Remains = append(p.Remains, Range{
+				First: nextIP(ip),
+				Last:  r.Last,
+			})
+			p.Remains[i].Last = prevIP(ip)
+			p.Clean()
+			return nil
+		}
+	}
+	return fmt.Errorf("%s is already used", ip.String())
+}
+
+func (r *Range) Count() int64 {
+	count := int64(0)
+	for i := 0; i < net.IPv6len; i++ {
+		count = (count << 8) + int64(r.Last[i]-r.First[i])
+	}
+	return count + 1
+}
+
+func (r *Range) Contain(ip net.IP) bool {
+	for i := 0; i < net.IPv6len; i++ {
+		if ip[i] < r.First[i] || r.Last[i] < ip[i] {
+			return false
+		}
+	}
+	return true
+}
---
+++ ippool_test.go
@@ -0,0 +1,62 @@
+package ippool
+
+import (
+	"net"
+	"testing"
+)
+
+func TestRangeCount(t *testing.T) {
+	r := Range{
+		First: net.IPv4(1, 2, 0, 0),
+		Last:  net.IPv4(1, 3, 0, 0),
+	}
+	expect := int64(65537)
+	count := r.Count()
+	if count != expect {
+		t.Errorf("Range.Count() is %d (expect=%d)", expect, count)
+	}
+}
+
+func TestRangeContainTrue(t *testing.T) {
+	r := Range{
+		First: net.IPv4(1, 2, 0, 0),
+		Last:  net.IPv4(1, 3, 0, 0),
+	}
+	if !r.Contain(net.IPv4(1, 2, 0, 0)) {
+		t.Error("Range.Contain() is not true")
+	}
+}
+
+func TestRangeContainFalse(t *testing.T) {
+	r := Range{
+		First: net.IPv4(1, 2, 0, 0),
+		Last:  net.IPv4(1, 3, 0, 0),
+	}
+	if r.Contain(net.IPv4(1, 3, 0, 1)) {
+		t.Error("Range.Contain() is not false")
+	}
+}
+
+func TestPrevIP(t *testing.T) {
+	ip := net.IPv4(1, 0, 0, 0)
+	if !prevIP(ip).Equal(net.IPv4(0, 255, 255, 255)) {
+		t.Error()
+	}
+}
+
+func TestNextIP(t *testing.T) {
+	ip := net.IPv4(0, 255, 255, 255)
+	if !nextIP(ip).Equal(net.IPv4(1, 0, 0, 0)) {
+		t.Error()
+	}
+}
+
+func TestRangeAllocate(t *testing.T) {
+	pool := NewPool(
+		net.IPv4(10, 224, 0, 0),
+		net.IPv4(10, 224, 255, 255),
+	)
+	if err := pool.Allocate(net.IPv4(10, 224, 100, 1)); err != nil {
+		t.Error(err)
+	}
+}
